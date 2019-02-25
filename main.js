9e9d672197d1435c331d020a8ac742fb6192a967
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-02-25 16:29:30 +0000

    Added buttons

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://swapi.co/api/";
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
+function getData(type, cb) {
+    
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
 
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+    
+}
+
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    
+    getData(type, function(data) {
+        data = data.results;
+        data.forEach(function(item) {
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
+        
+    });
+}
